e894feb93cd89ac3d404c07295fff431416669c1
Hasan Masum <1805052@ugrad.cse.buet.ac.bd>
2023-06-05 02:07:38 +0600

    added view binding

--- app/src/main/java/com/pubablibank/pibanking/MainActivity.java
+++ app/src/main/java/com/pubablibank/pibanking/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.pubablibank.pibanking.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mVB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mVB = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mVB.getRoot());
     }
 }
